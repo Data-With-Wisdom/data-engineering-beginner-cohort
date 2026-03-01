40f784c9fb353a536e90470bcfeb779833c4db65
Nwachukwu Wisdom <wisdom.nwachukwu@tecnotree.com>
2026-03-01 08:56:39 +0100

    Added content for week 4

---
+++ 04-python-essentials-for-data-engineers/scripts/hello.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+print(f"Welcome to a simple python progam, {name} !")
+
+
+# C:\Users\nwachwi\Documents\per\data-engineering-beginner-cohort\04-python-essentials-for-data-engineers\scripts\hello.py
